68c94a34f8af2603515064a2007a7f7751499563
Eder <lg2xspeed@googlemail.com>
2021-04-12 09:54:56 +0200

    JS-24 Persisting sent mails

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongoModule } from 'src/providers/mongodb/mongo.module';
 import { MailService } from './mail.service';
 
 @Module({
-  imports: [],
+  imports: [MongoModule],
   providers: [MailService],
   exports: [MailService],
 })
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,26 +1,49 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { MailData } from './interfaces/mail-data.interface';
+import { Db } from 'mongodb';
+import { MailEntity } from './entities/mail.entity';
 
 @Injectable()
 export class MailService {
+  constructor(
+    @Inject('MONGO_CONNECTION')
+    private mongodb: Db,
+  ) {}
   private mailerService: MailerService;
 
   //send a job offer to student and replace template text with handlebars (context)
-  async sendJobOffer(mailData: MailData) {
-    await this.mailerService.sendMail({
-      to: mailData.to,
-      subject: 'subject',
-      text: `text`,
-      template: 'example',
-      context: {
-        title: mailData.title,
-        url: mailData.url,
-        actionTitle: 'Anfrage akzeptieren',
-        app_name: 'Studentenjobbörse',
-        text1: mailData.text1,
-        text2: mailData.text2,
-      },
-    });
+  async sendJobOffer(mailData: MailData, mailEntity: MailEntity) {
+    return this.mailerService
+      .sendMail({
+        to: mailData.to,
+        subject: 'subject',
+        text: `text`,
+        template: 'example',
+        context: {
+          title: mailData.title,
+          url: mailData.url,
+          actionTitle: 'Anfrage akzeptieren',
+          app_name: 'Studentenjobbörse',
+          text1: mailData.text1,
+          text2: mailData.text2,
+        },
+      })
+      .catch((err) => {
+        throw err;
+      })
+      .then(async () => {
+        return this.mongodb
+          .collection('mails')
+          .insertOne(mailEntity)
+          .catch((err) => {
+            console.log(err);
+            throw new InternalServerErrorException();
+          });
+      });
   }
 }
