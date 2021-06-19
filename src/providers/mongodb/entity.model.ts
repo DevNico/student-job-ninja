6e351e7bdc694861641ceb5046e85c2c3a5fae3e
Eder <lg2xspeed@googlemail.com>
2021-06-19 16:01:35 +0200

    fixed: missing id property (swagger)

--- src/providers/mongodb/entity.model.ts
+++ src/providers/mongodb/entity.model.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Entity {
+  @ApiProperty({
+    description: 'id',
+    type: String,
+  })
   _id: string;
   constructor(_id: string) {
     this._id = _id;
